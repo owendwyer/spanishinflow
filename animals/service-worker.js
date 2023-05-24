12bd8aaef76f229f44bd600b69b3f8b1a06513da
Owen Dwyer <odwyer@gmail.com>
2023-05-24 14:28:27 +0700

    no fetch in online

--- animals/service-worker.js
+++ animals/service-worker.js
@@ -1,9 +1,4 @@
 self.addEventListener("install", event => {
     self.skipWaiting();
-    console.log('ON installed online sw')
-});
-
-self.addEventListener("fetch", (e) => {
-    console.log('ON fetch in online sw')
-    return;
+    console.log('ON installed online sw - no fetch')
 });
